abc686dc589248dd0a7dc397cbbd123be2f96761
Max  Kelly <max.diamond.kelly@gmail.com>
2024-08-12 09:51:03 +1000

    add skeleton folder structure

---
+++ dj_ai_2/orchestrator.py
@@ -0,0 +1,7 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+## TODO
+## sound = AudioSegment.from_file("bin/track/MDX22.mp3", format="mp3")
+## play(sound)
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+import dj_ai_2.orchestrator 
+
+def main():
+    print("howdy")
+
+if __name__ == "__main__":
+    main()
